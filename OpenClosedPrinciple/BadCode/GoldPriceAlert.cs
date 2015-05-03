276d620523e8b72dddd68a0072960f42d68303cc
unknown <iulian.tabara@BlueOne.meshdigital.net>
2015-05-03 22:55:57 +0300

    Open/Closed Principle

---
+++ OpenClosedPrinciple/BadCode/GoldPriceAlert.cs
@@ -0,0 +1,26 @@
+﻿using SingleResponsibilityPrinciple.RefactoredCode;
+using System;
+
+namespace OpenClosedPrinciple.BadCode
+{
+    public class GoldPriceAlert
+    {
+        public void ShowLowGoldPriceAlert(GoldMeterEnchanced goldmeter, LogType logType)
+        {
+            if (logType == LogType.Email)
+            {
+                // TODO: Send me the email
+                return;
+            }
+
+            Console.WriteLine("Better to buy some gold now: {0:F2}", goldmeter.GoldPrice);
+        }
+    }
+
+    public enum LogType
+    {
+        Console,
+        Email,
+        SMS
+    }
+}
---
+++ OpenClosedPrinciple/RefactoredCode/GoldPriceAlert.cs
@@ -0,0 +1,61 @@
+﻿using SingleResponsibilityPrinciple.RefactoredCode;
+using System;
+
+namespace OpenClosedPrinciple.RefactoredCode
+{
+    public interface ILog
+    {
+        void LogAlert(string message);
+    }
+
+    public class Log
+    {
+        ILog logger;
+
+        public Log(ILog logger)
+        {
+            this.logger = logger;
+        }
+
+        public void LogAlert(string message)
+        {
+            logger.LogAlert(message);
+        }
+    }
+
+    public class ConsoleAlert : ILog
+    {
+
+        public void LogAlert(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class EmailAlert : ILog
+    {
+
+        public void LogAlert(string message)
+        {
+            //TODO: Send me the email
+        }
+    }
+
+    public class SMSAlert : ILog
+    {
+
+        public void LogAlert(string message)
+        {
+            //TODO: Send me the SMS
+        }
+    }
+
+    public class GoldPriceAlert
+    {
+        public void ShowLowGoldPriceAlert(GoldMeterEnchanced goldmeter, ILog logger)
+        {
+            var message = string.Format("Better to buy some gold now: {0:F2}", goldmeter.GoldPrice);
+            logger.LogAlert(message);
+        }
+    }
+}
